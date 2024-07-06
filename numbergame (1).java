87104679cadaddb0ac824ee1c17b1ce3f7c6ff56
Tondare <snehatondare2019@gmail.com>
2024-07-06 17:24:38 +0530

    Add files via upload
    this is a number game project code

---
+++ numbergame (1).java
@@ -0,0 +1,52 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+import java.util.Scanner;
+ public class numbergame
+{
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int chances=4;
+	int finals=4;
+	boolean PlayAgain=true;
+	System.out.print("welcome gamer!");
+	System.out.println("Hey gamer you have about "+chances+" chances to win the game");
+	while(PlayAgain){
+	    int rand=getrandN(1,100);
+	    boolean guessNO=false;
+	    for(int i=0;i<chances;i++){
+	        System.out.println("chance "+(i+1)+" enter your guessNO:");
+	        int user=sc.nextInt();
+	        if (user==rand){
+	            guessNO=true;
+	            finals=4;
+	            System.out.println("congrats you won the game.");
+	            break;
+	        }
+	        else if(user>rand){
+	            System.out.println("Too High number");
+	        }
+	        else{
+	            System.out.println(" Too Low number");
+	        }
+	    }
+	    if (guessNO==false){
+	        System.out.println("oops..sorry gamer. you lost the chances. the number is "+rand);
+	    }
+	    
+	    System.out.println("Do you want to play Again(Yes/No)");
+	       String pA=sc.next();
+	    PlayAgain= pA.equalsIgnoreCase("Yes");
+	 }
+	 System.out.println("That's it gamer ...Hope you enjoyed this game");
+	 System.out.println("Here is your score" +finals);
+	 }
+	 public static int getrandN(int min,int max){
+     	   return(int)(Math.random()*(max-min+1)+min);
+	   }
+}
